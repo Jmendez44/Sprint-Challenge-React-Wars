2510dcd10710584a212aa4e4623c1438e55966ba
jmendez44 <jenuel.mendez@gmail.com>
2019-02-15 12:52:40 -0500

    list items for each character created

--- starwars/src/App.jsx
+++ starwars/src/App.jsx
@@ -6,9 +6,23 @@ const Character = props => {
   const {birth_year, created, edited, eye_color, films, gender, hair_color, height, homeworld, mass, name, skin_color, species, starships, url, vehicle} = props.charProp;
   return (
     <div>
-      <h4>Character: {name} </h4>
+      <h2> {name} </h2>
       <ul>
-        <li>Birth Year: {birth_year}</li>
+        <li><h5>Birth Year: </h5> {birth_year} </li>
+        <li><h5>Created: </h5> {created} </li>
+        <li><h5>Edited: </h5> {edited} </li>
+        <li><h5>Eye Color: </h5> {eye_color} </li>
+        <li><h5>Films: </h5> {films} </li>
+        <li><h5>Gender: </h5> {gender} </li>
+        <li><h5>Hair Color: </h5> {hair_color} </li>
+        <li><h5>Height: </h5> {height} </li>
+        <li><h5>Home World: </h5> {homeworld} </li>
+        <li><h5>Mass: </h5> {mass} </li>
+        <li><h5>Skin Color: </h5> {skin_color} </li>
+        <li><h5>Species: </h5> {species} </li>
+        <li><h5>Starships: </h5> {starships} </li>
+        <li><h5>Created: </h5> {url} </li>
+        <li><h5>Vehicle: </h5> {vehicle} </li>
       </ul>
     </div>
   )
